86b4b610376dede48cea417ab4438b4d40151228
Hugo Mauricio Morales Acevedo <58606889+Hugo-Morales@users.noreply.github.com>
2022-03-08 14:07:44 -0300

    Se arreglo el componente Error 404

--- src/views/Error 404/error.jsx
+++ src/views/Error 404/error.jsx
@@ -1,38 +1,19 @@
 import { Link } from "react-router-dom";
-import Styles from "../landingpage/landingpage.module.css";
+
 const Error404 = () => {
     return (
-        <div
-            className="
-        flex
-        items-center
-        justify-center
-        w-screen
-        h-screen
-        bg-gradient-to-r
-        
-        "
-            className={Styles.container}
-
-
-        >
+        <div className="flex items-center justify-center w-screen h-screen container_000">
             <div className="px-40 py-20 bg-white rounded-md shadow-xl">
                 <div className="flex flex-col items-center">
-
                     <h1 className="font-bold text-blue-600 text-9xl">404</h1>
-
                     <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
-                        <span className="text-red-500">Oops!</span> Page not found
+                        <span className="text-red-500">¡Oops!</span> Página no encontrada
                     </h6>
-
                     <p className="mb-8 text-center text-gray-500 md:text-lg">
-                        The page you’re looking for doesn’t exist.
+                        La página que buscas no existe.
                     </p>
-
                     <Link to="/home">
-
-                        <div href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-indigo-700"> Go to Home </div>
-
+                        <div href="#" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-600 hover:bg-indigo-700"> Volver al inicio </div>
                     </Link>
                 </div>
             </div>
