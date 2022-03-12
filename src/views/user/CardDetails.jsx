efd91ca94d7662bc0330da1e59b8c5f203f630d6
Peccosbill <jonathanmontini95@gmail.com>
2022-03-12 18:39:39 -0300

    Acomodando componentes en CardDetail

--- src/views/user/CardDetails.jsx
+++ src/views/user/CardDetails.jsx
@@ -56,30 +56,6 @@ const CardDetail = ({ handleAddToCart }) => {
                     </svg>
                   </div>
                 </li>
-
-                <li>
-                  <div className="flex items-center">
-                    <Link
-                      to="#"
-                      className="mr-2 text-sm font-medium text-gray-900"
-                    >
-                      {" "}
-                      {products?.description}{" "}
-                    </Link>
-                    <svg
-                      width="16"
-                      height="20"
-                      viewBox="0 0 16 20"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      className="w-4 h-5 text-gray-300"
-                    >
-                      <path d="M5.697 4.34L8.98 16.532h1.327L7.025 4.341H5.697z" />
-                    </svg>
-                  </div>
-                </li>
-
                 <li className="text-sm">
                   <Link
                     to="#"
@@ -135,77 +111,16 @@ const CardDetail = ({ handleAddToCart }) => {
 
               <div className="mt-4 lg:mt-0 lg:row-span-3">
                 {/* <h2 className="sr-only"></h2> */}
-                <p className="text-3xl text-gray-900">${products?.price}</p>
-
-                <div className="mt-6">
-                  <h3 className="sr-only">Reviews</h3>
-                  <div className="flex items-center">
-                    <div className="flex items-center">
-                      <svg
-                        className="text-gray-900 h-5 w-5 flex-shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-
-                      <svg
-                        className="text-gray-900 h-5 w-5 flex-shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-
-                      <svg
-                        className="text-gray-900 h-5 w-5 flex-shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-
-                      <svg
-                        className="text-gray-900 h-5 w-5 flex-shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-
-                      <svg
-                        className="text-gray-200 h-5 w-5 flex-shrink-0"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-                      </svg>
-                    </div>
-                    <p className="sr-only">3 out of 5 stars</p>
-                    <Link
-                      to="#"
-                      className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500"
-                    >
-                      117 reviews
-                    </Link>
-                  </div>
+                <div className="flex justify-between items-center">
+                  <p className="text-3xl text-gray-900">${products?.price}</p>
+                  <p className="text-3xl inline-block font-bold text-white p-2 bg-green-500 rounded-sm">
+                    {products?.discount}
+                    <span className="">% Off</span>
+                  </p>
                 </div>
 
                 <form className="mt-10">
                   {/* <h2 className="sr-only"></h2> */}
-                  <p className="text-3xl text-gray-900">
-                    {products?.discount} %Off
-                  </p>
 
                   <button
                     onClick={() => handleAddToCart(products)}
@@ -229,44 +144,14 @@ const CardDetail = ({ handleAddToCart }) => {
                 </div>
 
                 <div className="mt-10">
-                  <h3 className="text-sm font-medium text-gray-900">
-                    Highlights
-                  </h3>
-
-                  <div className="mt-4">
-                    <ul className="pl-4 list-disc text-sm space-y-2">
-                      <li className="text-gray-400">
-                        <span className="text-gray-600">
-                          Hand cut and sewn locally
-                        </span>
-                      </li>
-
-                      <li className="text-gray-400">
-                        <span className="text-gray-600">
-                          Dyed with our proprietary colors
-                        </span>
-                      </li>
-
-                      <li className="text-gray-400">
-                        <span className="text-gray-600">
-                          Pre-washed &amp; pre-shrunk
-                        </span>
-                      </li>
-
-                      <li className="text-gray-400">
-                        <span className="text-gray-600">
-                          Ultra-soft 100% cotton
-                        </span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-
-                <div className="mt-10">
-                  <h2 className="text-sm font-medium text-gray-900">Details</h2>
+                  <h2 className="text-xl font-bold text-gray-900">
+                    Detaille del producto
+                  </h2>
 
                   <div className="mt-4 space-y-6">
-                    <p className="text-sm text-gray-600">La mejor comida</p>
+                    <p className="text-md text-gray-600">
+                      {products?.description}
+                    </p>
                   </div>
                 </div>
               </div>
