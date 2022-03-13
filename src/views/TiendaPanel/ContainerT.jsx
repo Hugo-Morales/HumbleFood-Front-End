0aeb5d1da86a6b3596a00edecfbea9a278bd4f7d
diemora473 <diego.e.morales01@gmail.com>
2022-03-13 18:15:46 -0300

    se rompio todoo

--- src/views/TiendaPanel/ContainerT.jsx
+++ src/views/TiendaPanel/ContainerT.jsx
@@ -13,28 +13,6 @@ import {
 } from "../../redux/actions";
 
 export default function ContainerT({ user }) {
-<<<<<<< HEAD
-	const [id, setId] = useState("");
-	const [currentPage, setCurrentPage] = useState(0);
-	const dispatch = useDispatch();
-	const productos = useSelector((state) => state.productLoaded);
-	const cargando = useSelector((state) => state.isLoading);
-	const usuario = useSelector((state) => state.dataUser);
-
-	const allproducts = useSelector((state) => state.productsloaded);
-	const { userId } = useParams();
-	const pages =
-		usuario?.rol === 2 ? allproducts.pagesTotal : productos.pagesTotal;
-	// console.log(user, 'user');
-	// console.log(currentPage);
-
-
-	const paging = (num) => {
-		if (num >= 0 && num <= pages) {
-			setCurrentPage(num);
-		}
-	};
-=======
   const [id, setId] = useState("");
   const [currentPage, setCurrentPage] = useState(0);
   const productos = useSelector((state) => state.productsloaded);
@@ -55,7 +33,6 @@ export default function ContainerT({ user }) {
   useEffect(() => {
     dispatch(loading());
     dispatch(getdataUser(userId));
->>>>>>> master
 
     if (usuario?.rol === 2) {
       dispatch(getallproducts(currentPage));
@@ -69,47 +46,6 @@ export default function ContainerT({ user }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, userId, currentPage, usuario.rol]);
 
-<<<<<<< HEAD
-		return () => {
-			dispatch(reset());
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [dispatch, userId, currentPage, usuario.rol]);
-
-	return (
-		<>
-			{cargando ? (
-				<Loading />
-			) : (
-				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-4">
-					{/* Lado Izquierdo */}
-					<div className="col-span-1">
-						<SideLeft
-							name={user.name}
-							setId={setId}
-							rol={usuario.rol}
-							shopsId={usuario.shopsId}
-						/>
-					</div>
-					{/* Lado Derecho */}
-					<div className="col-span-3">
-						<SideRight
-							product={usuario.rol === 2 ? allproducts : productos}
-							idS={id}
-							shopsId={usuario?.shopsId}
-							paging={paging}
-							currentPage={currentPage}
-							next={productos.next}
-							prev={productos.prev}
-							pagesTotal={productos.pagesTotal}
-						/>
-					</div>
-				</div>
-			)}
-		</>
-	);
-
-=======
   return (
     <>
       {cargando ? (
@@ -142,5 +78,4 @@ export default function ContainerT({ user }) {
       )}
     </>
   );
->>>>>>> master
 }
