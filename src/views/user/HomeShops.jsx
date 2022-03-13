3bfbb0e0297074a4e6852591b2b1945a0ddaa317
Hugo Mauricio Morales Acevedo <58606889+Hugo-Morales@users.noreply.github.com>
2022-03-13 19:23:38 -0300

    Se agrego paginado a shop, se arreglo los tipeos en los buttons

--- src/views/user/HomeShops.jsx
+++ src/views/user/HomeShops.jsx
@@ -1,48 +1,63 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import NavShop from "../../components/shops/NavShops";
 import Carousell from "../../components/carousell/Carousell";
 import CardsShops from "../../components/shops/CardsShops";
 import Loading from "../../components/loading/Loading";
+import Paginado from "../../components/paginado/Paginado";
 import { getShops, loading, postnewUser } from "../../redux/actions";
 
 export default function HomeShops() {
-  const dispatch = useDispatch();
-  const { isAuthenticated, user, isLoading } = useAuth0();
-  const shops = useSelector((state) => state.shops);
-  const cargando = useSelector((state) => state.isLoading);
+	const dispatch = useDispatch();
+	const { isAuthenticated, user, isLoading } = useAuth0();
+	const shops = useSelector((state) => state.shops);
+	const cargando = useSelector((state) => state.isLoading);
+	const [currentPage, setCurrentPage] = useState(0);
 
-  const newUser = {
-    userId: user?.sub.split("|")[1],
-    name: user?.name,
-    name_user: user?.nickname,
-    email: user?.email,
-    direction: "",
-  };
+	const paging = (num) => {
+		if (num >= 0 && num <= shops.pagesTotal) {
+			setCurrentPage(num);
+		}
+	};
 
-  useEffect(() => {
-    if (isAuthenticated && user) dispatch(postnewUser(newUser));
-  });
+	const newUser = {
+		userId: user?.sub.split("|")[1],
+		name: user?.name,
+		name_user: user?.nickname,
+		email: user?.email,
+		direction: "",
+	};
 
-  useEffect(() => {
-    dispatch(loading());
-    dispatch(getShops());
-  }, [dispatch]);
+	useEffect(() => {
+		if (isAuthenticated && user) dispatch(postnewUser(newUser));
+	});
 
-  if (isLoading) return <Loading />;
+	useEffect(() => {
+		dispatch(loading());
+		dispatch(getShops(currentPage));
+	}, [dispatch, currentPage]);
 
-  return (
-    <>
-      {cargando ? (
-        <Loading />
-      ) : (
-        <>
-          <NavShop />
-          <Carousell />
-          <CardsShops shops={shops.shops} />
-        </>
-      )}
-    </>
-  );
+	if (isLoading) return <Loading />;
+
+	return (
+		<>
+			{cargando ? (
+				<Loading />
+			) : (
+				<>
+					<NavShop />
+					<Carousell />
+					<CardsShops shops={shops.shops} />
+					<Paginado
+						next={shops.next}
+						prev={shops.prev}
+						pagesTotal={shops.pagesTotal}
+						paging={paging}
+						currentPage={currentPage}
+					/>
+				</>
+			)}
+		</>
+	);
 }
