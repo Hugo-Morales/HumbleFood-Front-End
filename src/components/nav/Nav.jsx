e46e5b25b5d881b5ff06516136300860c69b8ade
Peccosbill <jonathanmontini95@gmail.com>
2022-03-21 17:39:49 -0300

    Arreglo la posición del boton del carrito en versión mobile

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -155,11 +155,11 @@ const Nav = ({
       <div
         className={
           isAuthenticated
-            ? "w-1/4 flex justify-center items-center mr-5"
-            : "w-1/6 flex justify-beetwen items-center mr-8"
+            ? "w-1/4 flex justify-center items-center mr-6"
+            : "w-1/6 flex justify-beetwen items-center mr-16"
         }
       >
-        <Menu as="div" className="mt-1 mx-4 relative">
+        <Menu as="div" className="mt-1 mr-4 relative">
           <div className="flex justify-center items-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -191,7 +191,7 @@ const Nav = ({
             ) : (
               <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                 <img
-                  className="h-10 w-10 rounded-full"
+                  className="h-12 w-12 rounded-full"
                   src={userRojo}
                   alt="img not found"
                 />
